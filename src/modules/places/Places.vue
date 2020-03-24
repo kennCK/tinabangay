bfd28d4142d383ef6f1d71a903e1ee117c331cae
Kennette Canales <kennettecanales@gmail.com>
2020-03-24 14:25:41 +0800

    update

--- src/modules/places/Places.vue
+++ src/modules/places/Places.vue
@@ -1,55 +1,154 @@
 <template>
   <div>
-    <button class="btn btn-primary pull-right" style="margin-bottom: 25px; margin-top: 25px;">New Place</button>
+    <button class="btn btn-primary pull-right" style="margin-bottom: 25px; margin-top: 25px;" @click="showModal('create', null)">New Place</button>
+    <button class="btn btn-primary" @click="hideMessage()">Hide Message</button>
+    <h1 v-if="messageFlag === true">{{message}}</h1>
+    <h2 v-else>You hide me: {{message}}</h2>
     <table class="table table-responsive table-bordered">
-      <thead>
-        <td>Location</td>
-        <td>Date</td>
-        <td>Time</td>
+      <thead class="custom-header-color">
+        <td>Coutry</td>
+        <td>Region</td>
+        <td>Locality</td>
+        <td>Action</td>
       </thead>
       <tbody>
         <tr v-for="(item, index) in data" :key="index">
-          <td></td>
-          <td></td>
-          <td></td>
+          <td class="text-warning">{{item.country}}</td>
+          <td class="text-danger">{{item.region}}</td>
+          <td class="text-primary">{{item.locality === 'testin' ? 'true' : item.locality}}</td>
+          <td>
+            <button class="btn btn-primary" @click="showModal('update', item)">
+              <i class="fas fa-edit"></i>
+            </button>
+
+            <button class="btn btn-danger" @click="removeItem(item.id)">
+              <i class="fas fa-trash"></i>
+            </button>
+          </td>
         </tr>
       </tbody>
     </table>
+    <increment-modal :property="modalProperty"></increment-modal>
   </div>
 </template>
 <style lang="scss" scoped> 
 @import "~assets/style/colors.scss";
+.custom-header-color{
+  color: $primary;
+}
 </style>
 <script>
 import ROUTER from 'src/router'
 import AUTH from 'src/services/auth'
 import COMMON from 'src/common.js'
+import ModalProperty from './CreatePlaces.js'
 export default {
   mounted(){
+    this.retrieve()
   },
   data(){
     return {
       common: COMMON,
-      user: AUTH.user
+      user: AUTH.user,
+      modalProperty: ModalProperty,
+      data: null,
+      message: 'Test message',
+      messageFlag: true
     }
+  },
+  components: {
+    'increment-modal': require('components/increment/generic/modal/Modal.vue')
   },
   methods: {
     redirect(parameter){
       ROUTER.push(parameter)
     },
+    hideMessage(){
+      this.messageFlag = false
+    },
     retrieve(){
       let parameter = {
         condition: [{
           clause: '=',
-          column: 'acccount_id',
-          value: this.user.value
-        }]
+          column: 'account_id',
+          value: this.user.userID
+        }],
+        sort: {
+          locality: 'desc'
+        }
       }
       $('#loading').css({display: 'block'})
-      this.APIRequest('visited_places', parameter).then(response => {
+      this.APIRequest('visited_places/retrieve', parameter).then(response => {
         $('#loading').css({display: 'none'})
         this.data = response.data
       })
+    },
+    removeItem(id){
+      let parameter = {
+        id: id
+      }
+      $('#loading').css({display: 'block'})
+      this.APIRequest('visited_places/delete', parameter).then(response => {
+        this.retrieve()
+      })
+    },
+    showModal(action, item = null){
+      switch(action){
+        case 'create':
+          this.modalProperty = {...ModalProperty}
+          let inputs = this.modalProperty.inputs
+          inputs.map(input => {
+            input.value = null
+          })
+          this.modalProperty.params[0].value = this.user.userID
+          break
+        case 'update':
+          let modalData = {...this.modalProperty}
+          let parameter = {
+            title: 'Update Requests',
+            route: 'visited_places/update',
+            button: {
+              left: 'Cancel',
+              right: 'Update'
+            },
+            sort: {
+              column: 'created_at',
+              value: 'desc'
+            },
+            params: [{
+              variable: 'id',
+              value: item.id
+            }]
+          }
+          modalData = {...modalData, ...parameter} // updated data without
+          let object = Object.keys(item)
+          modalData.inputs.map(data => {
+            if(data.variable === 'longitude'){
+              data.value = item.longitude
+            }
+            if(data.variable === 'latitude'){
+              data.value = item.latitude
+            }
+            if(data.variable === 'country'){
+              data.value = item.country
+            }
+            if(data.variable === 'locality'){
+              data.value = item.locality
+            }
+            if(data.variable === 'region'){
+              data.value = item.region
+            }
+            if(data.variable === 'date'){
+              data.value = item.date
+            }
+            if(data.variable === 'time'){
+              data.value = item.time
+            }
+          })
+          this.modalProperty = {...modalData}
+          break
+      }
+      $('#createPlacesModal').modal('show')
     }
   }
 }
