45b8d026c12e8612ef76b3868d6ae73cd2ca258c
Kennette Canales <kennettecanales@gmail.com>
2020-03-24 14:54:33 +0800

    udpate

--- src/modules/dashboard/Dashboard.vue
+++ src/modules/dashboard/Dashboard.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
-    Page content goes here.
+    <div class="row">
+      <div class="col-lg-6">
+        <trend></trend>
+      </div>
+    </div>
   </div>
 </template>
+<style scoped>
+.half-rule {
+  margin-left: 0;
+  text-align: left;
+  width: 50%;
+}
+</style>
 <script>
   import Vue from 'vue'
   export default{
@@ -19,15 +30,10 @@
     },
     props: {
     },
+    components: {
+      'trend': require('modules/places/Trend.vue')
+    },
     methods: {
     }
-
   }
 </script>
-<style scoped>
-.half-rule {
-  margin-left: 0;
-  text-align: left;
-  width: 50%;
-}
-</style>
