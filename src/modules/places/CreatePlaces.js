aad98eb4a5578fd884cfd3d105e6683a9b48350e
Monster Dev <40906882+themonstrousdev@users.noreply.github.com>
2020-03-24 20:38:19 +0800

    input

--- src/modules/places/CreatePlaces.js
+++ src/modules/places/CreatePlaces.js
@@ -3,7 +3,47 @@ export default {
   size: 'modal-md',
   title: 'Add Places',
   background: null,
-  inputs: [],
+  inputs: [{
+    row: 'full',
+    label: 'Location',
+    variable: 'location',
+    placeholder: 'Set location',
+    value: null,
+    required: true,
+    id: 'latitude',
+    type: 'location',
+    inputType: 'text',
+    validation: {
+      size: 1,
+      type: 'location'
+    }
+  }, {
+    row: 'full',
+    label: 'Date',
+    variable: 'date',
+    placeholder: 'Enter Date',
+    value: null,
+    id: 'date',
+    type: 'input',
+    inputType: 'date',
+    validation: {
+      size: 1,
+      type: 'date'
+    }
+  }, {
+    row: 'full',
+    label: 'Time',
+    variable: 'time',
+    placeholder: 'Enter Time',
+    value: null,
+    id: 'time',
+    type: 'input',
+    inputType: 'time',
+    validation: {
+      size: 1,
+      type: 'time'
+    }
+  }],
   route: 'visited_places/create',
   button: {
     left: 'Cancel',
