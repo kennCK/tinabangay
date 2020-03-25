e44c5529c458a2db84648f63a827737cb4aeda90
Kennette Canales <kennettecanales@gmail.com>
2020-03-25 18:44:12 +0800

    update

--- src/modules/home/Landing/Contributors.vue
+++ src/modules/home/Landing/Contributors.vue
@@ -121,7 +121,7 @@ export default {
   data(){
     return {
       data: [
-        {name: 'Kennette Conales', org: 'Increment Technologies | University of San Carlos', position: 'Owner | Teacher', img: require('assets/img/contributors/kennette.jpg')},
+        {name: 'Kennette Canales', org: 'Increment Technologies | University of San Carlos', position: 'Owner | Teacher', img: require('assets/img/contributors/kennette.jpg')},
         {name: 'Patrick Elalto', org: 'University of San Carlos', position: 'Teacher', img: require('assets/img/contributors/patrick.jpg')},
         {name: 'Yol Torres', org: 'University of San Carlos | Passerelles Numeriques Philippines', position: 'Student', img: require('assets/img/contributors/yol.jpg')},
         {name: 'Elsie Kathryn Tagupa', org: 'University of San Carlos', position: 'Student', img: require('assets/img/contributors/elle.jpg')},
