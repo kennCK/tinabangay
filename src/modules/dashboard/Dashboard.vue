9c591c8b7cdb71ab27362070055dc7b0b2448cd8
Kennette Canales <kennettecanales@gmail.com>
2020-03-25 16:24:09 +0800

    update dashboard

--- src/modules/dashboard/Dashboard.vue
+++ src/modules/dashboard/Dashboard.vue
@@ -1,13 +1,25 @@
 <template>
-  <div>
-    <div class="row">
-      <div class="col-lg-6">
-        <!-- For trend places -->
+  <div style="margin-bottom: 200px;">
+    <div class="row" style="margin-top: 25px;">
+      <div class="col-lg-6" style="margin-bottom: 25px;">
+        <p>
+          Hi <b class="text-primary">{{user.username}}</b>! Below is your qr code. Show this to frontliners everytime they read your temperature.
+        </p>
+        <qr-code></qr-code>
+      </div>
+      <div class="col-lg-6" style="margin-bottom: 25px;">
+        <p>
+          List of places visited by affected individual
+        </p>
         <trend></trend>
       </div>
-      <div>
-        <!-- For temperature -->
-        <trend></trend>
+    </div>
+    <div class="row" style="margin-bottom: 25px;">
+      <div class="col-lg-6">
+        <p>
+          Summary of temperature within Locality
+        </p>
+        <temperature-summary></temperature-summary>
       </div>
     </div>
   </div>
@@ -20,25 +32,23 @@
 }
 </style>
 <script>
-  import Vue from 'vue'
-  export default{
-    name: '',
-    create(){
-
-    },
-    mounted(){
-    },
-    data(){
-      return {
-
-      }
-    },
-    props: {
-    },
-    components: {
-      'trend': require('modules/places/Trend.vue')
-    },
-    methods: {
+import ROUTER from 'src/router'
+import AUTH from 'src/services/auth'
+import COMMON from 'src/common.js'
+export default{
+  data(){
+    return {
+      user: AUTH.user
     }
+  },
+  props: {
+  },
+  components: {
+    'trend': require('modules/places/Trend.vue'),
+    'qr-code': require('modules/dashboard/QrCode.vue'),
+    'temperature-summary': require('modules/temperature/Summary.vue')
+  },
+  methods: {
   }
+}
 </script>
