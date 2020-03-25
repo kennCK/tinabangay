8c86ff5c81321d3de62a7bd24bd8c201077c8a1b
HTundra <58643990+HTundra@users.noreply.github.com>
2020-03-25 12:53:27 +0800

    added donation description and button stuff

--- src/modules/home/Landing/Donations.vue
+++ src/modules/home/Landing/Donations.vue
@@ -10,12 +10,13 @@
     </div>
     <div class="holder d-flex flex-row justify-content-center my-4">
 
-      <a href="#" class="item text-light d-flex flex-column justify-content-center align-items-center py-5" :style="{'background': 'linear-gradient(to left, ' + item.backgroundColor+'50, '+item.backgroundColor+'c0), url('+item.backgroundImg+') center/cover no-repeat'}" v-for="item, index in data" v-if="data !== null">
+      <a :href="item.donateLink" class="item text-light d-flex flex-column justify-content-center align-items-center py-5" :style="{'background': 'linear-gradient(to left, ' + item.backgroundColor+'50, '+item.backgroundColor+'c0), url('+item.backgroundImg+') center/cover no-repeat'}" v-for="item, index in data" v-if="data !== null">
         <span class="icon text-center mb-1 mt-3">
           <div class="position-relative mx-auto" :style="{'-webkit-mask': 'url('+item.img+') center/50% no-repeat'}"></div>
         </span>
         <div class="description mx-5 px-3 text-center mt-5">
-          <button type="button" class="btn btn-dark btn-lg font-weight-bold">Donate</button>
+            <button type="button" class="btn btn-dark btn-lg font-weight-bold">Donate</button>
+            
         </div>
       </a>
       
@@ -23,8 +24,17 @@
         <span class="description text-center">
             <h2>Time is not on our side but you can be</h2>
             <p>
-              As more municipalities and people use <b>Bird's Eye</b> the cost for our domain and utilities also increase. We need help 
+              Birdseye is a non-profit and is run by students, faculty, and other volunteers at the University of San Carlos.<br>
+              We rely on donations to keep our site up and running and free for everyone to use.<br>
+              As more municipalities use <b>Birdseye</b>, our cost of operations increase. You can help our cause by donating to us, even the smallest donations help.<br>
+              <h4>Help keep Birdseye flying, donate today!</h4>
             </p>
+            <!-- <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_s-xclick" />
+            <input type="hidden" name="hosted_button_id" value="TR5GQAHN69NC4" />
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+            <img alt="" border="0" src="https://www.paypal.com/en_PH/i/scr/pixel.gif" width="1" height="1" />
+            </form> -->
          </span>
 	</div>
 </template>
@@ -88,9 +98,9 @@ export default {
   data(){
     return {
       data: [
-        {img: SETTINGS.page[0].paypal, backgroundColor: '#26b0ff', backgroundImg: require('assets/img/paypal-background.jpg')},
-        {img: SETTINGS.page[0].gcash, backgroundColor: '#297dfa', backgroundImg: require('assets/img/gcash-background.jpeg')},
-        {img: SETTINGS.page[0].paymaya, backgroundColor: '#1760ff', backgroundImg: require('assets/img/paymaya-background.png')}
+        {img: SETTINGS.page[0].paypal, backgroundColor: '#26b0ff', backgroundImg: require('assets/img/paypal-background.jpg'), donateLink: 'https://goo.gl/9S8VGb'},
+        {img: SETTINGS.page[0].gcash, backgroundColor: '#297dfa', backgroundImg: require('assets/img/gcash-background.jpeg'), donateLink: 'changeme'},
+        {img: SETTINGS.page[0].paymaya, backgroundColor: '#1760ff', backgroundImg: require('assets/img/paymaya-background.png'), donateLink: 'changeme'}
       ]
     }
   },
