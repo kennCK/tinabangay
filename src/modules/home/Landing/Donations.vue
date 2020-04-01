ae7a2215d3e52ccee63d542742185e340efeff7e
Monster Dev <40906882+themonstrousdev@users.noreply.github.com>
2020-04-01 17:04:57 +0800

    contact fb for donations

--- src/modules/home/Landing/Donations.vue
+++ src/modules/home/Landing/Donations.vue
@@ -10,7 +10,7 @@
     </div>
     <div class="holder d-flex flex-row justify-content-center my-4 row w-100 mx-0 p-0">
 
-      <a :href="item.donateLink" class="item text-light d-flex flex-column justify-content-center align-items-center py-5 col-lg-4 col-md-10" :style="{'background': 'linear-gradient(to left, ' + item.backgroundColor+'50, '+item.backgroundColor+'c0), url('+item.backgroundImg+') center/cover no-repeat'}" v-for="item, index in data" v-if="data !== null">
+      <!-- <a :href="item.donateLink" class="item text-light d-flex flex-column justify-content-center align-items-center py-5 col-lg-4 col-md-10" :style="{'background': 'linear-gradient(to left, ' + item.backgroundColor+'50, '+item.backgroundColor+'c0), url('+item.backgroundImg+') center/cover no-repeat'}" v-for="item, index in data" v-if="data !== null">
         <span class="icon text-center mb-1 mt-3">
           <div class="position-relative mx-auto" :style="{'-webkit-mask': 'url('+item.img+') center/auto 100% no-repeat'}"></div>
         </span>
@@ -18,7 +18,7 @@
             <button type="button" class="btn btn-dark btn-lg font-weight-bold">Donate</button>
             
         </div>
-      </a>
+      </a> -->
       
       <span class="description text-center mb-5 col-10 mt-4">
           <h2>Time is not on our side but you can be</h2>
@@ -27,6 +27,7 @@
             We rely on donations to keep our site up and running and free for everyone to use.<br>
             As more municipalities use <b>Birdseye</b>, our cost of operations increase. You can help our cause by donating to us, even the smallest donations help.<br>
             <h4>Help keep Birdseye flying, donate today!</h4>
+            <h4>Contact us through our <a href="https://www.facebook.com/birdseyeph/">Facebook Page</a></h4>
           </p>
         </span>
     </div>
