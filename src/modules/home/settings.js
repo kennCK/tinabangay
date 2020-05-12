115912b7db3421228a4f8cef4bfc616aae922740
Monster Dev <40906882+themonstrousdev@users.noreply.github.com>
2020-05-12 13:04:44 +0800

    fix typo

--- src/modules/home/settings.js
+++ src/modules/home/settings.js
@@ -1,8 +1,8 @@
 export default {
   page: [{
     title: 'BirdsEye: COVID-19 Contact Tracing',
-    subtitle: 'Trace places, transporation and individual easily!',
-    description: 'Centralize application for LGU to monitor and trace individuals using visited places, transporatation used, temperature and symptons reported!',
+    subtitle: 'Trace places, transportation and individuals easily!',
+    description: 'Centralize application for LGU to monitor and trace individuals using visited places, transportation used, temperature and symptoms reported!',
     image: require('assets/img/logo.svg'),
     template: 'right',
     background: '#005b96',
