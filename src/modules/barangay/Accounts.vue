012a6317ebbe9ad5d086a0dcb085caff19375c51
Monster Dev <40906882+themonstrousdev@users.noreply.github.com>
2020-05-15 17:28:22 +0800

    upload brgy logo

--- src/modules/barangay/Accounts.vue
+++ src/modules/barangay/Accounts.vue
@@ -2,7 +2,8 @@
   <div class="ledger-summary-container">
     <button class="btn btn-warning pull-right" style="margin: .5% 0;" @click="importFlag = 3">Import Visited Places</button>
     <button class="btn btn-danger pull-right mr-3" style="margin: .5% 0;" @click="importFlag = 2">Import Symptoms</button>
-    <button class="btn btn-primary pull-right mr-3" style="margin: .5% 0;" @click="importFlag = 1">Import Accounts</button>
+    <button class="btn btn-success pull-right mr-3" style="margin: .5% 0;" @click="importFlag = 1">Import Accounts</button>
+    <button class="btn btn-dark pull-right mr-3" style="margin: .5% 0;" @click="showModal('edit_clearance')">Edit Clearance Layout</button>
     <button class="btn btn-primary pull-right mr-3" style="margin: .5% 0;" @click="showModal('account')">New Account</button>
     <button class="btn btn-primary pull-left mr-3" style="margin: .5% 0;" @click="showBrgyCodes()">Barangay Codes</button>
     <div class="form-group" v-if="importFlag !== 0">
@@ -49,13 +50,13 @@
           </td>
           <td>{{item.account.email}}</td>
           <td>
-            <button class="btn btn-primary" @click="exportUser(item.account)">Export User</button>
+            <button class="btn btn-primary" v-if="item.account_id === user.userID && item.member !== user.userID" @click="exportUser(item.account)">Export User</button>
             <button class="btn btn-info" @click="exportClearance(item.account)">Export Clearance</button>
           </td>
           <td>
             <button class="btn btn-dark" @click="showModal('address', item.account.id)">Set Barangay</button>
             <button class="btn btn-success" @click="showModal('place', item.account.id)">Add Visited Place</button>
-            <button class="btn btn-warning" @click="showModal('user', item.account.id)">Edit User</button>
+            <button class="btn btn-warning" v-if="item.account_id === user.userID" @click="showModal('user', item.account.id)">Edit User</button>
           </td>
         </tr>
       </tbody>
@@ -128,6 +129,41 @@
         </div>
       </div>
     </div>
+
+    <!--MODAL FOR EDITING CLEARANCE LAYOUT-->
+    <div class="modal fade right" id="edit_clearance" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+     aria-hidden="true">
+      <div class="modal-dialog modal-side modal-notify modal-primary modal-md" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Edit Clearance Layout</h5>
+            <button type="button" class="close" aria-label="Close" @click="hideModal('edit_clearance')">
+              <span aria-hidden="true" class="white-text">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body p-4">
+            <h5>Barangay Logo</h5>
+            <div class="row mb-3 mx-0 align-items-start">
+              <div class="col-5">
+                <img :src="brgy_logo ? brgy_logo : require('src/assets/img/no_image.png')" alt="Barangay Logo" class="img-fluid">
+              </div>
+              <div class="col-3" v-if="user.location">
+                <button class="btn btn-primary" @click="showModal('browseImagesModal')">Upload Logo</button>
+              </div>
+              <div class="col-7" v-if="!user.location">
+                <span class="text-danger">Please join a barangay before uploading a barangay logo</span>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button class="btn btn-danger" @click="hideModal('edit_clearance')">Cancel</button>
+            <button class="btn btn-primary" @click="addLoc()">Submit</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <images></images>
 
     <!--MODAL FOR ADDING ADDRESS-->
     <div class="modal fade right" id="add_location" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
@@ -289,6 +325,8 @@ export default{
       user: AUTH.user,
       data: null,
       brgy_codes: null,
+      brgy_logo: null,
+      withLogo: true,
       auth: AUTH,
       selecteditem: null,
       brgy: null,
@@ -386,6 +424,7 @@ export default{
     'empty': require('components/increment/generic/empty/Empty.vue'),
     'basic-filter': require('components/increment/generic/filter/Basic.vue'),
     'increment-modal': require('components/increment/generic/modal/Modal.vue'),
+    'images': require('components/increment/generic/image/BrowseModal.vue'),
     Pager
   },
   methods: {
@@ -421,15 +460,92 @@ export default{
         offset: (this.activePage > 0) ? this.activePage - 1 : this.activePage
       }
 
-      this.APIRequest('sub_accounts/retrieve', parameter).then(response => {
-        if(response.data.length > 0){
-          this.data = response.data
-          this.numPages = parseInt(response.size / this.limit) + (response.size % this.limit ? 1 : 0)
-        }else{
-          this.data = null
-          this.numPages = null
+      let param = {
+        condition: [{
+          value: this.user.location.code,
+          column: 'code',
+          clause: '='
+        }]
+      }
+
+      if(this.user.location && this.user.location.code) {
+        let brgyImgPar = {
+          condition: [{
+            value: this.user.location.code,
+            column: 'code',
+            clause: '='
+          }]
         }
-        $('#loading').css({display: 'none'})
+        this.APIRequest('brgy_codes/retrieve', brgyImgPar).then(response => {
+          if(response.data.length > 0) {
+            this.brgy_logo = CONFIG.BACKEND_URL + response.data[0].url
+          }
+        })
+      }
+      this.APIRequest('locations/retrieve', param).then(res => {
+        this.APIRequest('sub_accounts/retrieve', parameter).then(response => {
+          if(response.data.length > 0){
+            this.data = response.data
+            this.numPages = parseInt(response.size / this.limit) + (response.size % this.limit ? 1 : 0)
+          }else{
+            this.data = null
+            this.numPages = null
+          }
+          if(res.data.length > 0) {
+            res.data.map(loc => {
+              let flag
+              this.data.map(acc => {
+                if(acc.account_id === loc.account_id) {
+                  flag = true
+                }
+              })
+              if(!flag) {
+                let par = {
+                  condition: [{
+                    value: loc.account_id,
+                    column: 'account_id',
+                    clause: '='
+                  }]
+                }
+                $('#loading').css({display: 'block'})
+                this.APIRequest('sub_accounts/retrieve', par).then(resp => {
+                  if(resp.data.length > 0) {
+                    resp.data.map(newAcc => {
+                      this.data.push(newAcc)
+                    })
+                  }
+                  $('#loading').css({display: 'none'})
+                })
+              }
+            })
+          }
+        })
+      })
+
+    },
+    manageImageUrl(imageURL) {
+      let par = {
+        condition: [{
+          value: this.user.location.code,
+          column: 'code',
+          clause: '='
+        }]
+      }
+      $('#loading').css({display: 'block'})
+      this.APIRequest('brgy_codes/retrieve', par).then(response => {
+        if(response.data.length > 0) {
+          let id = response.data[0].id
+          let param = {
+            id: id,
+            url: imageURL
+          }
+          this.APIRequest('brgy_codes/update', param).then(res => {
+            $('#loading').css({display: 'none'})
+            this.brgy_logo = CONFIG.BACKEND_URL + imageURL
+          })
+        } else {
+          $('#loading').css({display: 'none'})
+        }
       })
     },
     retrieveBrgyCodes(sort){
@@ -638,6 +754,9 @@ export default{
         }
       })
     },
+    getBrgyLogo() {
+
+    },
     exportClearance(user) {
       $('#loading').css({display: 'block'})
       if(user.location === null) {
@@ -652,9 +771,9 @@ export default{
           }]
         }
         this.APIRequest('account_informations/retrieve', par).then(response => {
-          $('#loading').css({display: 'none'})
           if(response.data.length > 0) {
             let data = response.data[0]
+            let brgyImg = this.brgy_logo
             let image = require('assets/img/logo.png')
             let brgy = user.location
             let xhr = new XMLHttpRequest()
@@ -663,13 +782,55 @@ export default{
             xhr.onload = function() {
               let reader = new FileReader()
               reader.onloadend = function() {
+                let cont = [
+                  ' ',
+                  ' ',
+                  {
+                    text: 'CLEARANCE REPORT',
+                    bold: true,
+                    fontSize: 20,
+                    alignment: 'center'
+                  },
+                  ' ',
+                  {
+                    text: 'To whom it may concern:',
+                    fontSize: 14
+                  },
+                  ' ',
+                  'This is to certify that the following individual is clear from any contact. Please see the given details below',
+                  ' ',
+                  {
+                    text: user.username,
+                    fontSize: 15,
+                    bold: true,
+                    alignment: 'center'
+                  },
+                  ' ',
+                  {
+                    qr: user.code,
+                    alignment: 'center'
+                  },
+                  ' ',
+                  ' ',
+                  {
+                    text: `${data.first_name !== null && data.last_name !== null ? `${data.first_name.toUpperCase()} ${data.last_name.toUpperCase()}` : ''}`,
+                    italics: true,
+                    bold: true,
+                    decoration: 'underline'
+                  },
+                  'BARANGAY OFFICIAL',
+                  `${brgy.route}, ${brgy.locality}, ${brgy.region}`,
+                  ' ',
+                  ' ',
+                  `EXPORTED ON: ${moment().format('MMMM DD, YYYY :: hh:mm:ss A')}`
+                ]
                 let pdf = {
                   content: [
                     {
                       alignment: 'left',
                       columns: [
                         {
-                          image: 'logo',
+                          image: 'birdseyeLogo',
                           width: 50
                         },
                         [
@@ -695,50 +856,9 @@ export default{
                           }
                         ]
                       ]
-                    },
-                    ' ',
-                    ' ',
-                    {
-                      text: 'CLEARANCE REPORT',
-                      bold: true,
-                      fontSize: 20,
-                      alignment: 'center'
-                    },
-                    ' ',
-                    {
-                      text: 'To whom it may concern:',
-                      fontSize: 14
-                    },
-                    ' ',
-                    'This is to certify that the following individual is clear from any contact. Please see the given details below',
-                    ' ',
-                    {
-                      text: user.username,
-                      fontSize: 15,
-                      bold: true,
-                      alignment: 'center'
-                    },
-                    ' ',
-                    {
-                      qr: user.code,
-                      alignment: 'center'
-                    },
-                    ' ',
-                    ' ',
-                    {
-                      text: `${data.first_name !== null && data.last_name !== null ? `${data.first_name.toUpperCase()} ${data.last_name.toUpperCase()}` : ''}`,
-                      italics: true,
-                      bold: true,
-                      decoration: 'underline'
-                    },
-                    'BARANGAY OFFICIAL',
-                    `${brgy.route}, ${brgy.locality}, ${brgy.region}`,
-                    ' ',
-                    ' ',
-                    `EXPORTED ON: ${moment().format('MMMM DD, YYYY :: hh:mm:ss A')}`
-                  ],
+                    }],
                   images: {
-                    logo: `${reader.result}`
+                    birdseyeLogo: `${reader.result}`
                   },
                   styles: {
                     header: {
@@ -748,7 +868,35 @@ export default{
                     }
                   }
                 }
-                PdfPrinter.createPdf(pdf).download(`${user.username}_clearance.pdf`)
+                if(brgyImg !== null && brgyImg !== undefined) {
+                  fetch(brgyImg)
+                  .then(res => {
+                    return res.blob()
+                  })
+                  .then(blob => {
+                    let read = new FileReader()
+                    read.onloadend = function() {
+                      pdf.images.brgyLogo = `${read.result}`
+                      pdf.content.push({
+                        image: 'brgyLogo',
+                        width: 150,
+                        alignment: 'center'
+                      })
+                      cont.map(pdfContent => {
+                        pdf.content.push(pdfContent)
+                      })
+                      $('#loading').css({display: 'none'})
+                      PdfPrinter.createPdf(pdf).download(`${user.username}_clearance.pdf`)
+                    }
+                    read.readAsDataURL(blob)
+                  })
+                } else {
+                  cont.map(pdfContent => {
+                    pdf.content.push(pdfContent)
+                  })
+                  $('#loading').css({display: 'none'})
+                  PdfPrinter.createPdf(pdf).download(`${user.username}_clearance.pdf`)
+                }
               }
               reader.readAsDataURL(xhr.response)
             }
