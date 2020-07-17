9d914950fc3ff52297bf43c240fd240f6e9bee77
2ndyrGroupC <2ndyrGroupC@pn.local>
2020-07-17 10:10:53 +0800

    changing some designs

--- src/modules/plasma/plasma.vue
+++ src/modules/plasma/plasma.vue
@@ -1,19 +1,29 @@
 <template>
-  <div class="container">
-     <div v-if="data.length > 0" class="container">
-         <div class="container">
-            <textarea class="form-control textarea" v-model="post" placeholder="Type your POST here... Please Include Your Address" rows="15" id="comment"></textarea>
-            <button class="btn btn-primary postBtn" v-if="goingToPost">Post</button>
+  <div class="container" style="background-color:#DCDCDC">
+     <div v-if="data.length > 0" class="container"><br>
+         <div class="card" style="width:96%;margin-left:3vh">
+            <div class="row">
+                 <div>
+                  <i class="fa fa-user-circle-o profile-icon i-style"></i>
+                </div>
+                 <div class="col-lg-10">
+                    <textarea class="form-control textarea" v-model="post" placeholder="Type your POST here... Please Include Your Address" rows="15" id="comment" ref="textarea"></textarea>
+                </div>
+                 <div class="col-sm-1">
+                    <button class="btn btn-primary postBtn" :hidden="!post.length > 0">Post</button>
+                </div>
+            </div>
         </div>
             <div class="row">
                 <div class="column" v-for="(datus, index) in data" :key="index">
-                    <div class="card">
+                    <div class="card cards">
+                        <i class='fas fa-ellipsis-v ellipsis'></i>
                         <div style="align-items:center;display:flex">
                             <i class="fa fa-user-circle-o profile-icon i-style"></i>
                             <h4 class="h4Style">{{datus.username}}<br/>
                                 <p class="p-date">{{datus.date}}</p>
-                            </h4>               
-                        </div><hr/>
+                            </h4>         
+                        </div>
                      <p class="p-message">{{datus.message}}</p>
                     </div><br/>
                 </div><br/>
@@ -49,6 +59,11 @@
         font-size:50px;
         color:#005b96;
     }
+    .ellipsis{
+        position: absolute;
+        right: 0;
+        width: 20px;
+    }
     .p-date {
         font-size: 12px;
         position: relative;
@@ -56,7 +71,6 @@
     }
     .p-message {
         font-size: 15px;
-        text-align:center;
         padding-bottom:5vh
     }
     .div-contain {
@@ -66,43 +80,44 @@
         margin-top: 2%;
         width: 70%;
     }
-    i {
-        font-size: 30px;
-    }
+   
     * {
-  box-sizing: border-box;
-}
+        box-sizing: border-box;
+    }
 
-body {
-  font-family: Arial, Helvetica, sans-serif;
-}
+    body {
+        font-family: Arial, Helvetica, sans-serif;
+    }
 
-/* Remove extra left and right margins, due to padding */
-.row {margin: 10px;}
+    /* Remove extra left and right margins, due to padding */
+    .row {margin: 10px;}
 
-/* Clear floats after the columns */
-.row:after {
-  content: "";
-  display: table;
-  clear: both;
-}
+    /* Clear floats after the columns */
+    .row:after {
+    content: "";
+    display: table;
+    clear: both;
+    }
 
-/* Responsive columns */
-@media screen and (max-width: 600px) {
-  .column {
-    width: 100%;
-    display: block;
-    margin-bottom: 20px;
-  }
-}
-.card {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  padding: 16px;
-  background-color: #f1f1f1;
-}
-.textarea{
-    height:10vh
-}
+    /* Responsive columns */
+    @media screen and (max-width: 600px) {
+    .column {
+        width: 100%;
+        display: block;
+        margin-bottom: 20px;
+    }
+    }
+    .cards {
+        padding: 16px;
+    
+    }
+    .textarea{
+        height:10vh;
+        border-color:white;
+    }
+    .textareas{
+        width:80vh
+    }
 
 </style>
 
@@ -132,11 +147,6 @@ export default{
     'google-map-modal': require('components/increment/generic/map/ModalGeneric.vue')
   },
   mounted(){
-    if(this.post === ''){
-      this.goingToPost = false
-    }else{
-      this.goingToPost = true
-    }
   },
   methods: {
     addPost(){
