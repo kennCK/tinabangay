819fa552707768534387994bceb2f03844ae4b99
Monster Dev <40906882+themonstrousdev@users.noreply.github.com>
2020-07-17 21:35:02 +0800

    Who dies?

--- src/modules/dashboard/Business.vue
+++ src/modules/dashboard/Business.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <div v-if="businessInfo !== null" class="col-12 row pl-5 mt-3 align-items-center">
+    <div v-if="businessInfo !== null" class="w-100 row m-0 mt-3 align-items-center">
       <div class="logo">
         <img :src="backend+businessInfo.logo" alt="" class="img-fluid rounded-circle border border-primary border-thick">
       </div>
-      <div class="col-auto row flex-column ml-1">
+      <div class="business-details row flex-column">
         <h3 class="text-primary font-weight-bold">{{businessInfo.name}}</h3>
         <div class="text-secondary font-weight-bold">{{businessInfo.email}}</div>
         <div class="text-secondary">{{businessInfo.address}}</div>
@@ -15,8 +15,9 @@
       <div class="col-12 text-center">
         <h5 class="text-primary font-weight-bold">Dashboard</h5>
       </div>
-      <div class="col-6 mt-3">
-        <qr-code-scanner :state="qrScannerState" @toggleState="(newState) => qrScannerState = newState"></qr-code-scanner>
+      <div class="col-md-6 col-sm-12 mt-3">
+        <qr-code-scanner :btnWidth="'col-md-3 col-sm-12'" :state="qrScannerState" @toggleState="(newState) => qrScannerState = newState"></qr-code-scanner>
+
       </div>
       <div class="col-6 mt-3">
         
@@ -26,12 +27,32 @@
 </template>
 <style scoped lang="scss">
   @import "~assets/style/colors.scss";
-  .logo {
-    max-width: 10%;
-  }
-
   .border-thick {
     border-width: 5px !important;
+  }
+
+  .logo {
+    max-width: 30%;
+    margin: 0 auto;
+  }
+
+  .business-details {
+    width: 100%;
+    text-align: center;
+    margin: 0;
+  }
+
+  @media (min-width: 768px) {  
+    .logo {
+      max-width: 10%;
+      margin-left: 3rem;
+      margin-right: 1rem;
+    }
+
+    .business-details {
+      width: auto;
+      text-align: left;
+    }
   }
 </style>
 <script>
