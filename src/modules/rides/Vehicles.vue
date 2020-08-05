717bd31db2b7d06e233799083464804cbe1717d7
Monster Dev <40906882+themonstrousdev@users.noreply.github.com>
2020-08-05 10:31:38 +0800

    vehicles

---
+++ src/modules/rides/Vehicles.vue
@@ -0,0 +1,20 @@
+<template>
+  <div >
+    Please lang ko ani
+  </div>
+</template>
+<style lang="scss">
+  @import "~assets/style/colors.scss";
+</style>
+<script>
+import ROUTER from 'src/router'
+import AUTH from 'src/services/auth'
+import COMMON from 'src/common.js'
+import CONFIG from 'src/config.js'
+import Pager from 'src/components/increment/generic/pager/Pager.vue'
+import QrcodeVue from 'qrcode.vue'
+import showQrCode from './ShowMore.vue'
+export default {
+  
+}
+</script>
